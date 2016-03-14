329911a5968775d64c9e20a6f1d5f9eeddc93b2f
Amandeep Chawla <amandeep@adobe.com>
2016-03-14 14:13:00 +0530

    Using Open GL to draw a red background

--- HelloOpenGL/ViewController.h
+++ HelloOpenGL/ViewController.h
@@ -7,8 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : GLKViewController {
+    EAGLContext* context;
+}
 
 
 @end
--- HelloOpenGL/ViewController.m
+++ HelloOpenGL/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+-(void) initGL;
 @end
 
 @implementation ViewController
@@ -17,6 +17,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // initialize the rendering context for OpenGL ES 2
+    context = [ [EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    
+    // associate the context with the GLKView
+    GLKView* view = (GLKView*) self.view;
+    view.context = context;
+    
+    // make the context current
+    [EAGLContext setCurrentContext:context];
+    
+    [self initGL];
+}
+
+-(void) initGL {
+    // set the clear color
+    glClearColor( 1.0, 0.0, 0.0, 1.0 );
+    glClearDepthf(1.0);
+}
+
+-(void) glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    // rendering function
+    
+    //clear the color buffer
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_DEPTH_BUFFER_BIT);
+    
+    //flush the opengl pipeline so that the commands get set to GPU
+    glFlush();
 }
 
 - (void)didReceiveMemoryWarning {
