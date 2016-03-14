f31ed5d6176526afee2dc2c415bf94a801c07732
Amandeep Chawla <amandeep@adobe.com>
2016-03-14 12:46:19 +0530

    Initial Commit

---
+++ HelloOpenGL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloOpenGL
+//
+//  Created by Amandeep Chawla on 14/03/16.
+//  Copyright (c) 2016 Amandeep Chawla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloOpenGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloOpenGL
+//
+//  Created by Amandeep Chawla on 14/03/16.
+//  Copyright (c) 2016 Amandeep Chawla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
